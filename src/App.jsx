e518c141083ae1f5c6e9d0c2557e558749c5a518
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-12 11:53:12 -0700

    Install Nova React library and import styles library at the root level of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import './globals.css';
-
 const App = () => {};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import '@visa/nova-styles/styles.css';
+import '@visa/nova-styles/themes/visa-light/index.css';
+import './globals.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
