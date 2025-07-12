8d0bcb77effb225f5c8c974087f191bb33e2ba4a
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-12 15:37:09 -0700

    Add responsiveness to the header
    This commit changes the UI for the header on larger screens. Comments were added to note future implementations.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,29 @@
 import { Nav } from '@visa/nova-react';
-import { GenericMenuTiny } from '@visa/nova-icons-react';
 import { Avatar } from '@visa/nova-react';
+import { GenericMenuTiny } from '@visa/nova-icons-react';
+import { GenericWriteLow } from '@visa/nova-icons-react';
 
 const Header = () => {
   const user = 'Hannah Kim';
   const userInitials = 'HK';
 
   return (
-    <Nav>
-      <GenericMenuTiny />
-      <a href="/" aria-label="Envisa home page">
+    <Nav className="flex justify-between">
+      {/* TODO: add sidebar implementation */}
+      <GenericMenuTiny className="lg:hidden" />
+      <a href="/" aria-label="Envisa home page" className="text-2xl font-bold">
         Envisa
       </a>
-      {/* TODO: turn into clickable avatar with account menu */}
-      <Avatar role="img" aria-label={user} small>
-        {userInitials}
-      </Avatar>
+      {/* TODO: add dropdown for nav links */}
+      <div className="flex items-center gap-4">
+        <a href="/" aria-label="Start new search" className="hidden lg:block">
+          <GenericWriteLow />
+        </a>
+        {/* TODO: turn into clickable avatar with account menu */}
+        <Avatar role="img" aria-label={user} small>
+          {userInitials}
+        </Avatar>
+      </div>
     </Nav>
   );
 };
