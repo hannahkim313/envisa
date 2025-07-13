4e72f384c6ef89c92f5bb4c7167f195971be495f
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-13 16:07:55 -0700

    Fix border radius not applying to bottom corners of code snippet

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -3,31 +3,33 @@ import CopyToClipboard from './CopyToClipboard';
 import clsx from 'clsx';
 
 const CodeBlock = ({ code }) => (
-  <div className="bg-code-block-accent flex w-full flex-col overflow-visible rounded-md">
-    <div className="self-end pt-2 pr-4 pb-2 pl-2">
-      <CopyToClipboard textToCopy="Copy" hasIcon />
+  <div className="bg-code-block-accent relative rounded-md shadow-md">
+    <div className="flex justify-end pt-2 pr-4 pb-2 pl-2">
+      <CopyToClipboard textToCopy="Copy" hasIcon={true} />
     </div>
 
-    <div className="w-full overflow-x-auto">
-      <Highlight code={code.trim()} language="jsx" theme={themes.nightOwl}>
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre
-            className={clsx(className, 'min-w-fit p-4 text-sm')}
-            style={style}
-          >
-            {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({ line })}>
-                <span className="inline-block w-8 pr-4 text-right">
-                  {i + 1}
-                </span>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token })} />
-                ))}
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
+    <div className="overflow-hidden rounded-b-md">
+      <div className="w-full overflow-x-auto">
+        <Highlight code={code.trim()} language="jsx" theme={themes.nightOwl}>
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre
+              className={clsx(className, 'min-w-fit p-4 text-sm')}
+              style={style}
+            >
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({ line })}>
+                  <span className="inline-block w-8 pr-4 text-right">
+                    {i + 1}
+                  </span>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+      </div>
     </div>
   </div>
 );
