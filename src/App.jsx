bb9e11b8d4c3a7857801b6e6f4dcec10b2c281c7
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-12 17:02:08 -0700

    Define the grid structure of the app and define the placement of the header
    This commit defines a simple grid layout for the app and updates the header to be placed in the respective grid row.
    
    This commit also updates a div to visa's utility component for consistency and adds spacing between groups of code for improved readability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-const App = () => {};
+// import MainPage from './components/MainPage';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div className="grid min-h-screen grid-rows-[auto_1fr]">
+      <Header />
+      <main>
+        {/* TODO: create the main page */}
+        {/* <MainPage /> */}
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Nav } from '@visa/nova-react';
+import { Nav, Utility } from '@visa/nova-react';
 import { Avatar } from '@visa/nova-react';
 import { GenericMenuTiny } from '@visa/nova-icons-react';
 import { GenericWriteLow } from '@visa/nova-icons-react';
@@ -8,22 +8,25 @@ const Header = () => {
   const userInitials = 'HK';
 
   return (
-    <Nav className="flex justify-between">
+    <Nav className="row-start-1 row-end-2 flex justify-between">
       {/* TODO: add sidebar implementation */}
       <GenericMenuTiny className="lg:hidden" />
+
       <a href="/" aria-label="Envisa home page" className="text-2xl font-bold">
         Envisa
       </a>
+
       {/* TODO: add dropdown for nav links */}
-      <div className="flex items-center gap-4">
+      <Utility vFlex vAlignItems="center" vGap={16}>
         <a href="/" aria-label="Start new search" className="hidden lg:block">
           <GenericWriteLow />
         </a>
+
         {/* TODO: turn into clickable avatar with account menu */}
         <Avatar role="img" aria-label={user} small>
           {userInitials}
         </Avatar>
-      </div>
+      </Utility>
     </Nav>
   );
 };
