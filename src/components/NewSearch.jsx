998fb552eb3508aef90384a0b22c9bed7d23d544
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-12 21:57:10 -0700

    Extract current UI for starting a new search into separate component for scalability

---
+++ src/components/NewSearch.jsx
@@ -0,0 +1,78 @@
+import { GenericSendLow } from '@visa/nova-icons-react';
+import {
+  Button,
+  Input,
+  InputContainer,
+  InputMessage,
+  Label,
+  Typography,
+  Utility,
+  UtilityFragment,
+} from '@visa/nova-react';
+
+const NewSearch = () => {
+  const id = 'component-keywords';
+
+  return (
+    <Utility
+      vPadding={16}
+      vFlex
+      vFlexCol
+      vJustifyContent="between"
+      className="row-start-2 row-end-3 h-full justify-center md:gap-12 md:p-5 lg:p-6"
+    >
+      <Utility
+        vFlex
+        vFlexGrow
+        vFlexCol
+        vAlignItems="center"
+        vJustifyContent="center"
+        vGap={8}
+        className="mb-[30vh] text-center md:mb-0 md:grow-0 md:gap-6"
+      >
+        <Typography variant="headline-1" tag="h1" className="md:text-5xl">
+          Hi, Hannah 👋
+        </Typography>
+        <Typography variant="body-2-bold" className="md:text-xl">
+          What are we building today?
+        </Typography>
+      </Utility>
+
+      <UtilityFragment
+        vElevation="xsmall"
+        className="mx-auto w-full rounded-2xl md:mb-[30vh] md:max-w-3xl"
+      >
+        <form action="">
+          <Label htmlFor={id} className="sr-only">
+            Component keywords (required)
+          </Label>
+
+          <InputContainer className="h-12 rounded-2xl p-4">
+            <Input
+              id={id}
+              name="component-keywords"
+              type="text"
+              aria-required="true"
+              aria-describedby={`${id}-message`}
+            />
+            <Button
+              type="submit"
+              aria-label="Submit your message"
+              iconButton={true}
+              buttonSize="small"
+              colorScheme="tertiary"
+            >
+              <GenericSendLow />
+            </Button>
+          </InputContainer>
+
+          <InputMessage id={`${id}-message`} className="sr-only">
+            Describe your component or idea in a few keywords.
+          </InputMessage>
+        </form>
+      </UtilityFragment>
+    </Utility>
+  );
+};
+
+export default NewSearch;
