4a4249ddb42a8bdaac19407e7c6a1d77929d3d7d
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-14 14:59:59 -0700

    Refactor the header by making UI elements clickable to simulate UX flow

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,5 @@
-import { Nav, Utility } from '@visa/nova-react';
-import { Avatar } from '@visa/nova-react';
-import { GenericMenuTiny } from '@visa/nova-icons-react';
-import { GenericWriteLow } from '@visa/nova-icons-react';
+import { Nav, Utility, Avatar, Button } from '@visa/nova-react';
+import { GenericWriteLow, GenericMenuLow } from '@visa/nova-icons-react';
 
 const Header = () => {
   const user = 'Hannah Kim';
@@ -9,20 +7,27 @@ const Header = () => {
 
   return (
     <Nav className="row-start-1 row-end-2 flex justify-between">
-      {/* TODO: add sidebar implementation */}
-      <GenericMenuTiny className="lg:hidden" />
+      <Button colorScheme="tertiary" className="lg:hidden">
+        <GenericMenuLow />
+      </Button>
 
       <a href="/" aria-label="Envisa home page" className="text-2xl font-bold">
         Envisa
       </a>
 
-      {/* TODO: add dropdown for nav links */}
       <Utility vFlex vAlignItems="center" vGap={16}>
-        <a href="/" aria-label="Start new search" className="hidden lg:block">
-          <GenericWriteLow />
-        </a>
+        <Button
+          colorScheme="tertiary"
+          buttonSize="large"
+          element={
+            <a href="/">
+              <GenericWriteLow />
+            </a>
+          }
+          aria-label="Start new search"
+          className="hidden px-4 lg:flex"
+        />
 
-        {/* TODO: turn into clickable avatar with account menu */}
         <Avatar role="img" aria-label={user} small>
           {userInitials}
         </Avatar>
